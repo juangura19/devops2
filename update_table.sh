50fdd531f7a9dd83bd75b27600257ad89565f115
Juan GUTIÉRREZ RAMOS <juangura19@gmail.com>
2023-03-16 12:04:47 -0500

    add date log & save file log.txt

--- update_table.sh
+++ update_table.sh
@@ -2,7 +2,8 @@
 
 if mysql -u root -p'root' -e "USE ${1} ; DESCRIBE ${2};" >/dev/null 2>&1; then
   mysql -u root -p'root' -e "TRUNCATE TABLE ${1}.${2}"
+  echo "$(date) SUCCESS - TRUNCATE TABLE ${1}.${2}"  >> /home/juan/Documentos/log.txt
 else
-  echo "La tabla ${1}.${2} no existe en la base de datos ${1}"
+  echo "$(date) ERROR - La tabla ${1}.${2} no existe en la base de datos ${1}"  >> /home/juan/Documentos/log.txt
 fi
 
