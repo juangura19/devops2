d3a09a7fb78fa4276e27c10eeb8cf6f0974dddc1
Juan GUTIÉRREZ RAMOS <juangura19@gmail.com>
2023-03-16 12:04:15 -0500

    add sentence truncate table

--- update_table.sh
+++ update_table.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-
+if mysql -u root -p'root' -e "USE ${1} ; DESCRIBE ${2};" >/dev/null 2>&1; then
+  mysql -u root -p'root' -e "TRUNCATE TABLE ${1}.${2}"
+else
+  echo "La tabla ${1}.${2} no existe en la base de datos ${1}"
+fi
 
